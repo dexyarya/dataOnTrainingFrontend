5c7d19ddb359803fe0169f1a9f63316fb7c09cec
dexyarya <arya.dexy@gmail.com>
2022-08-01 16:29:42 +0700

    login page

---
+++ src/components/login/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="login">
+      <h1>Image</h1>
+      <h1>Form Login</h1>
+    </div>
+  );
+};
+
+export default Login;
